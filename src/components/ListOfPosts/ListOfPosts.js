7f4aa05afb979253e48fd02003972cd18c43d444
Raphael Adams <raphael.adams@shopify.com>
2020-12-16 10:44:22 -0500

    Basic styling, will revisit later

--- src/components/ListOfPosts/ListOfPosts.js
+++ src/components/ListOfPosts/ListOfPosts.js
@@ -1,16 +1,14 @@
 import React, { Component } from "react";
 import './ListOfPosts.css';
+import Post from '../Post';
 
 class ListOfPosts extends Component {
   render() {
-    return <li id="micropost-1">
-      {/* <%= link_to gravatar_for(micropost.user, size: 50), micropost.user %> */}
-      <span class="user">JOHN DOE</span>
-      <span class="content">LOREM IPSUM</span>
-      <span class="timestamp">
-        Posted TIME AGO IN WORDS ago.
-      </span>
-    </li>;
+    return <div className="Post">
+      <li><Post/></li>
+      <li><Post/></li>
+      <li><Post/></li>
+    </div>;
   }
 }
 
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
+import raphdp from './raphdp.png';
 
-const Post = () => {
-  return <div />;
-};
+class Post extends Component {
+  render() {
+    return <micropost>
+      <img src={raphdp} 
+        alt="display pic" 
+        class="gravatar"
+        style={{width: 50, height: 50}}/>
+      <span class="user">JOHN DOE</span>
+      <span class="content">Some test post text, nothing to see here</span>
+      <span class="timestamp">Posted 2 hours ago.</span>
+    </micropost>;
+  }
+}
 
 export default Post;
