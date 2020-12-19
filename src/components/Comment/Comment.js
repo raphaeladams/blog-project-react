91e33ee37273db49bf79e2a9193db74815af059b
Raphael Adams <raphael.adams@shopify.com>
2020-12-19 17:07:29 -0500

    Redesigned components for Post & CommentForm, rewrote as Functional

--- src/components/Comment/Comment.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Comment = () => {
-  return <div>Test comment text</div>;
-};
-
-export default Comment;
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,20 +1,37 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import raphdp from './raphdp.png';
-import Comment from '../Comment';
+import CommentForm from '../CommentForm';
 
-class Post extends Component {
-  render() {
-    return <micropost>
+
+export default function Post() {
+  const [commenting, setCommenting] = useState(false);
+  const [liked, setLiked] = useState(false);
+
+  return (
+    <micropost>
+
       <img src={raphdp}
         alt="display pic"
         className="gravatar"
-        style={{width: 50, height: 50}}/>
-      <span className="user">JOHN DOE</span>
-      <span className="content">Some test post text, nothing to see here</span>
-      <span className="timestamp">Posted 2 hours ago.</span>
-      <a href="http://shopify.com">See Comments</a>
-    </micropost>;
-  }
-}
+        style={{width: 120, height: 120}}/>
 
-export default Post;
+      <div className="user">JOHN DOE</div>
+      <br></br>
+      <div className="content">Some test post text, nothing to see here</div>
+      <div className="timestamp">Posted 2 hours ago.</div>
+      <br></br>
+
+      <button
+        onClick={() => setLiked(liked ? false : true)}>
+        {liked ? "Unlike" : "Like"}
+      </button>
+      
+      <button
+        onClick={() => setCommenting(commenting ? false : true)}>
+        {commenting ? "Post" : "Comment"}
+      </button>
+      {commenting && <CommentForm />}
+      
+    </micropost>
+  );
+}
