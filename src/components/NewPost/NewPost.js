d8ff15f6a39639ca0e2867c4694704d8530636b4
Raphael Adams <raphael.adams@shopify.com>
2021-01-04 11:43:41 -0500

    Finished backend & frontend implementation for GQL mutations

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,29 +1,60 @@
 import React, {useState, useCallback} from "react";
+import {useMutation} from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 import {
   Page,
   Card,
   FormLayout,
   TextField,
-  Button
+  Button,
+  Form
 } from '@shopify/polaris';
+
+const ADD_POST = gql`
+mutation CreateMicropost($content: String!) {
+  createMicropost(input: {micropostRequest: {content: $content, userId: 1}})
+  {
+    micropost {
+      id,
+      content,
+      createdAt,
+      user {
+        name,
+        id
+      }
+    }
+  }
+}`;
 
 export default function NewPost() {
   const [content, setContent] = useState('');
   const handleContentChange = useCallback((value) => setContent(value), []);
+  
+  const [addMicropost, {loading: mutationLoading, error: mutationError}] = useMutation(ADD_POST);
+
+  const handleSubmit = useCallback((_event) => {
+    addMicropost({ variables: {content: content} });
+    setContent('');
+  }, [content, addMicropost]);
 
   return (
     <Page title='New Post'>
       <Card sectioned>
-        <FormLayout>
-          <TextField
-            placeholder='Enter post content'
-            maxLength={140}
-            value={content}
-            onChange={handleContentChange}
-          />
+        <Form onSubmit={handleSubmit}>
+          <FormLayout>
+            <TextField
+              placeholder='Enter post content'
+              maxLength={140}
+              value={content}
+              onChange={handleContentChange}
+            />
 
-          <Button primary submit={true}>Post!</Button>
-        </FormLayout>
+            <Button primary submit>Post!</Button>
+
+            {mutationLoading && <p>Loading...</p>}
+            {mutationError && <p>Error :( Please try again!</p>}
+          </FormLayout>
+        </Form>
       </Card>
     </Page>
   );
